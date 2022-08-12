589b68f281ebc200ff4282c6e5a2fc203f980f57
Alexander Dimov <106152399+AlexDimov-04@users.noreply.github.com>
2022-08-12 20:32:07 +0300

    Add files via upload

---
+++ url_shortener.py
@@ -0,0 +1,9 @@
+# import module
+from pyshorteners import *
+
+# url link
+link = "https://en.wikipedia.org/wiki/Andrew_Tate"
+
+# output to be shortened
+output = Shortener().clckru.short(link)
+print(output)
